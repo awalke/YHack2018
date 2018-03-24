bdca011926a269b32197ee830df990b422cea552
twalke <tyler.walke@gmail.com>
2018-03-24 02:58:22 -0600

    manage shelter and dog profile

---
+++ PlaceHolderView-2.x/app/src/main/java/com/mindorks/demo/OurStuff/ManageDogProfile.java
@@ -0,0 +1,15 @@
+package com.mindorks.demo.OurStuff;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mindorks.demo.R;
+
+public class ManageDogProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_dog_profile);
+    }
+}
